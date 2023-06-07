fd9e6037aeb6a47aaf523cd00cbca4532ff11a2b
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-06-07 18:36:59 -0300

    Programa que busca CEP com Angular

--- aula_6-angular/apppegacep/src/app/app.component.ts
+++ aula_6-angular/apppegacep/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { HttpClient } from "@angular/common/http";
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-root',
@@ -7,44 +7,54 @@ import { HttpClient } from "@angular/common/http";
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'apppegacep';
-  constructor(private http: HttpClient) { }
-  //variaveis do form
-  cep: string = '';
-  //..variaveis do retorno
-  endereco;
-  retorno = {
-    rua: '',
-    cidade: '',
-    estado: ''
+  title = 'teste';
+
+  constructor(private http: HttpClient) {}
+
+  cep: string ='';
+
+  endereco : any;
+  retorno={
+    rua : '',
+    cidade : '',
+    estado : ''
   }
-  //logradoro=rua, localidade=cidade, uf=estado
+
+  //logradouro=rua, localidade=cidade, uf=estado
+
   pesquisado = false;
   espera = false;
-  pesquisa() {
-    this.espera = true;
-    this.cep = this.cep.replace(/\D/g, '');
-    const url = 'http://viacep.com.br/ws/'
-      + this.cep + '/json/';
 
-    // while(true) teste=0;
-    this.http.get(url).subscribe((res)=> {
+
+  pesquisa(){
+    this.espera = true;
+    this.cep=this.cep.replace(/\D/g, '');
+
+    const url = 'http://viacep.com.br/ws'+this.cep+'/json/';
+
+    this.http.get(url).subscribe( (res) =>{
       this.endereco = res;
+
       this.retorno.rua = this.endereco.logradouro;
-      this.retorno.cidade = this.endereco.localidade;
+      this.retorno.cidade  = this.endereco.localidade;
       this.retorno.estado = this.endereco.uf;
-      //console.log(this.endereco);
     });
     this.pesquisado = true;
     this.espera = false;
+
+    console.log(this.retorno.rua);
+
+
   }
-  limpa() {
-    this.pesquisado = false;
-    this.espera = false;
-    this.cep = "";
-    this.endereco = "";
-    this.retorno.rua = "";
-    this.retorno.cidade = "";
-    this.retorno.estado = "";
+
+  limpa(){
+    this.pesquisado=false;
+    this.espera=false;
+    this.cep="";
+    this.endereco="";
+    this.retorno.rua="";
+    this.retorno.cidade="";
+    this.retorno.estado="";
   }
+
 }
--- aula_6-angular/apppegacep/src/app/app.module.ts
+++ aula_6-angular/apppegacep/src/app/app.module.ts
@@ -1,19 +1,21 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+
+import {BrowserModule} from '@angular/platform-browser';
+import{NgModule} from '@angular/core';
 import {HttpClientModule} from '@angular/common/http';
-import { AppComponent } from './app.component';
 import{FormsModule} from '@angular/forms';
 
+import {AppComponent} from './app.component';
+
 @NgModule({
-  declarations: [
+ declarations: [
     AppComponent
-  ],
-  imports: [
+ ],
+ imports:[
     FormsModule,
     BrowserModule,
     HttpClientModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+ ],
+ providers: [],
+ bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule{}
